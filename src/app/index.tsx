cd7437c3a18555e7b8c3e3dca9df1aeecdd3241b
MATHEUS DONIZETI FERREIRA FRANCO <matheus.dffranco@senacsp.edu.br>
2024-07-22 22:15:30 -0300

    continuação da primeira tela

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,57 @@
+
+import { View, Text, FlatList, SectionList } from "react-native";
 import { Header } from "@/components/header";
-import { View, Text } from "react-native";
+import { Categorybutton } from "@/components/category-button";
+import { CATEGORIES, MENU } from "../../utils/data/products";
+import { useState } from "react";
 
 
-export default function Home(){
-    return(
+
+export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0])
+    function handleCategorySelect(selectedCategory: string) {
+        setCategory(selectedCategory)
+        const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory)
+    }
+
+    return (
         <View>
-            <Header title="Faça seu pedido"/>
-            <Header title="Sua sacolinha"/>
+            <Header title="Faça seu pedido" cartQuatityItems={0} />
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) =>
+                    <Categorybutton
+                        title={item}
+                        isSelected={item === category}
+                        onPress={() =>handleCategorySelect(item)}
+                    />
+                }
+                horizontal
+                className="max-h-10"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{gap:12, paddingHorizontal:20}}
+            />
+            <SectionList
+            sections={MENU}
+            keyExtractor={(item)=> item.id}
+            //não fazer efeito de esticar
+            stickySectionHeadersEnabled={false}
+            renderItem={({item})=> 
+            <Text className="text-white">{item.title}</Text>
+        }
+        renderSectionHeader={({section: {title}})=> 
+            <Text className="text-yellow-300">{title}</Text>
+    }
+            />
+
         </View>
     )
 }
+
+// <View className="flex-row gap-7">
+//     <Categorybutton  title="Lanche do dia"/>
+//     <Categorybutton  title="Promoções"/>
+//     <Categorybutton  title="Bebidas"/>
+//     <Categorybutton  title="Sobremesas"/>
+// </View>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,28 @@
-import { View, Text, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import colors from "tailwindcss/colors";
 
 type HeaderProps ={
     title: string
+    cartQuatityItems?: number
 }
 
-export function Header({title}: HeaderProps) {
+export function Header({title, cartQuatityItems = 0}: HeaderProps) {
     return(
         <View  className="flex-row items-center border-b border-slate-700 pb-5 mx-5 my-10">
             <View className="flex-1">
                 <Image source={require('@/assets/logo.png')} className="h-6 w-32"/>
                 <Text className="text-white text-xl font-heading mt-2">{title}</Text>
             </View>
+            
+            <TouchableOpacity className="relative" activeOpacity={0.5}>
+            {cartQuatityItems > 0 && (
+                <View className="bg-lime-400 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-4">
+                    <Text className="text-slate-900 font-bold text-xs">{cartQuatityItems}</Text>
+                </View>
+                )}
+                <Feather name="shopping-cart" color={colors.white} size={24}/>
+            </TouchableOpacity>
 
         </View>
     )
