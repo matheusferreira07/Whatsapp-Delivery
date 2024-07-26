b71e6e972946f12c777ada04d5681136489ae3b1
MATHEUS DONIZETI FERREIRA FRANCO <matheus.dffranco@senacsp.edu.br>
2024-07-25 21:29:24 -0300

    atualização da segunda tela

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,8 +1,11 @@
-import { MaterialIcons } from "@expo/vector-icons";
 import { router, useLocalSearchParams } from "expo-router";
 import { View, Text, Image } from "react-native";
 import { PRODUCTS } from "../../../utils/data/products";
-import { white } from "tailwindcss/colors";
+
+import { formatCurrency } from "../../../utils/functions/format-currency";
+import { LinkButton } from "@/components/link-button";
+import { Feather } from "@expo/vector-icons";
+import { Button } from "@/components/button";
 
 export default function Product() {
     const { id } = useLocalSearchParams()
@@ -13,22 +16,35 @@ export default function Product() {
 
     return (
         <View className="flex-1 mt-10">
-            <MaterialIcons
-            size={34}
-            name="arrow-back"
-            onPress={() => router.back}
-            color={white}
-            />
-            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
+
+
+            <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
             <View className="p-5 mt-8 flex-1">
-                <Text className="text-white">{product.title}</Text>
-                <Text className="text-white">{product.price}</Text>
-                <Text className="text-white">{product.description}</Text>
+                <Text className="text-white text-xl font-heading">
+                    {product.title}</Text>
+                <Text className="text-lime-400 text-2xl font-heading my-2">
+                    {formatCurrency(product.price)}</Text>
+                <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
+                {product.ingredients.map(ingredient => (
+                    <Text className="text-slate-400 font-body text-base leading-6" key={ingredient}>
+                        {'\u2022'} {ingredient}
+                    </Text>
+                ))}
 
 
             </View>
-
-
+            <View className="p-4 pb-8 gap-5">
+                <Button>
+                    <Button.Icon>
+                        <Feather name="plus-circle" size={20} />
+                    </Button.Icon>
+                    <Button.Text>
+                        Adicionar ao Pedido
+                    </Button.Text>
+                </Button>
+                <LinkButton title="Voltar ao cardápio" href="/" />
+            </View>
         </View>
     )
 }
+
